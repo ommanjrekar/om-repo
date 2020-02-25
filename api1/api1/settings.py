94d0d8c1aae69850555e48164b61e00e9dd56f6c
omkarmajrekar <omkar.manjrekar@intelliswift.co.in>
2020-02-25 12:29:00 +0530

    Configured postgres

--- api1/api1/settings.py
+++ api1/api1/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'api1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Om',
+        'USER':'postgres',
+        'PASSWORD':'postgres',
+        'HOST':'localhost',
     }
 }
 
