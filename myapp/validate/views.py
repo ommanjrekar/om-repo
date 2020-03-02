a3a34ad33a6c7ce4790a178a5ae4a5d36c41af9e
intelliswift <intelliswift@localhost.localdomain>
2020-03-02 11:41:37 -0500

    Updaed views

--- myapp/validate/views.py
+++ myapp/validate/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.models import User, auth
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
 # Create your views here.
 
 def register(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password1=request.POST['password1']
+        password2=request.POST['password2']
+
+        if password1==password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username exist')
+                return render(request, 'register.html')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'email taken')
+                return render(request, 'register.html')
+            else :
+                user = User.objects.create_user(username=username, email=email, password=password1)
+                user.save()
+                messages.info(request, 'User created')
+                return render (request, 'register.html')
+
+        else : 
+            messages.info(request, 'Password does not match')
+            return render (request, 'register.html')
+    else : 
+        return render(request, 'register.html')
+
+
+def register1(request):
     form = UserCreationForm()
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -12,9 +40,22 @@ def register(request):
             form.save()
 
     context = {'form': form}
-    return render(request, 'register.html')
+    return render(request, 'register.html', context)
 
 def login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None : 
+            auth.login(request, user)
+            messages.info(request, 'You are logged in')
+            return render (request, 'login.html')
+
+        else :
+            messages.info(request, 'Invalid credentials...')
+            return render (request, 'login.html')
     return render(request, 'login.html')
 
 
